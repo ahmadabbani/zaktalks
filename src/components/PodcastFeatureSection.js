d9b399caa3dd00d8032890eb1b886560b5246e1f
Ahmad Abbani <ahmdabbanyy@gmail.com>
2026-07-29 00:47:58 +0300

    Redesign homepage sections

--- src/components/PodcastFeatureSection.js
+++ src/components/PodcastFeatureSection.js
@@ -133,10 +133,10 @@ export default function PodcastFeatureSection() {
 
             <div className={styles.phoneWrap}>
               <Image
-                src="/podcast/zak-talks-phone-facing-copy.png"
+                src="/podcast/zak-talks-phone-facing-copy-v2.png"
                 alt="Modern phone playing The Conversations We Avoid on Zak Talks"
-                width={948}
-                height={1659}
+                width={941}
+                height={1672}
                 sizes="(max-width: 620px) 86vw, (max-width: 980px) 60vw, 34vw"
                 className={styles.phone}
               />
